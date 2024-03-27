5cdd059dd935155ce1c40904ec4b8dbb11811306
LavaCandy1 <aayush.garg.1793@gmail.com>
2024-03-28 00:06:20 +0530

    Del commit 28-03-24

---
+++ delete_slot.cpp
@@ -0,0 +1,32 @@
+#include "loader.cpp"
+// #include "input.cpp"
+#include <iostream>
+
+using namespace std;
+
+void delete_slot(string batch,int slot_no){
+    
+    multiset<Class> loaded_data = load_data(batch);
+
+    // Print the loaded data
+  for (const auto &slot : loaded_data) {
+    // cout << "Room no : " << slot.room_No << endl;
+    // cout << "Course Code : " << slot.course_Code << endl;
+    // cout << "Professor : " << slot.professor << endl;
+    // cout << "Group: " << slot.group << endl;
+    // cout << "Year: " << slot.year << endl;
+    // cout << "Time Slot: " << slot.time_Slot << endl;
+    // cout << "Day : " << slot.day << endl;
+    // cout << "Type of class : " << slot.type<<endl;
+    cout << "Slot_number : " << slot.slot_Num<<endl;
+
+      cout << "Type : " << slot.type << endl;
+    cout<<"\n\n\n\n\n";
+  }
+
+}
+
+
+int main(){
+    delete_slot("B40",1);
+}
