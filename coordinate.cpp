c37fd32b3c05c341ee5165d33323a76a136bb5df
LavaCandy1 <aayush.garg.1793@gmail.com>
2024-03-28 21:08:58 +0530

    a lil slot number allocation correction

--- coordinate.cpp
+++ coordinate.cpp
@@ -7,38 +7,38 @@ using namespace std;
 int getCoordinateNumber(const string& day, const string& timeSlot) {
     map<string, int> dayMap = {
         {"monday", 1}, {"tuesday", 2}, {"wednesday", 3}, {"thursday", 4},
-        {"friday", 5}
+        {"friday", 5}, {"saturday", 6}
     };
 
     map<string, int> timeSlotMap = {
-        {"08:30", 1}, {"09:30", 2}, {"10:30", 3}, {"11:30", 4},
-        {"13:30", 5}, {"14:30", 6}, {"15:30", 7}, {"16:30", 8}
+        {"08:30", 1}, {"09:30", 2}, {"10:30", 3}, {"11:30", 4}, {"12:30",5},
+        {"13:30", 6}, {"14:30", 7}, {"15:30", 8}, {"16:30", 9}, {"17:30",10}
     };
 
     if (dayMap.find(day) != dayMap.end() && timeSlotMap.find(timeSlot) != timeSlotMap.end()) {
         int dayNumber = dayMap[day];
         int timeSlotNumber = timeSlotMap[timeSlot];
-        return ((dayNumber - 1) * 8) + timeSlotNumber;
+        return ((dayNumber - 1) * timeSlotMap.size()) + timeSlotNumber;
     } else {
         return -1;
     }
 }
 
-// int main() {
-//     string inputDay, inputTimeSlot;
+int main() {
+    string inputDay, inputTimeSlot;
 
-//     cout << "Enter the day (e.g., Monday, Tuesday, etc.): ";
-//     getline(cin, inputDay);
+    cout << "Enter the day (e.g., Monday, Tuesday, etc.): ";
+    getline(cin, inputDay);
 
-//     cout << "Enter the time slot (e.g., 8:30, 9:30, etc.): ";
-//     getline(cin, inputTimeSlot);
+    cout << "Enter the time slot (e.g., 8:30, 9:30, etc.): ";
+    getline(cin, inputTimeSlot);
 
-//     int coordinateNumber = getCoordinateNumber(inputDay, inputTimeSlot);
-//     if (coordinateNumber != -1) {
-//         cout << "The coordinate/number for " << inputDay << " " << inputTimeSlot << " is: " << coordinateNumber << endl;
-//     } else {
-//         cout << "Invalid input! Please enter a valid day and time slot." << endl;
-//     }
+    int coordinateNumber = getCoordinateNumber(inputDay, inputTimeSlot);
+    if (coordinateNumber != -1) {
+        cout << "The coordinate/number for " << inputDay << " " << inputTimeSlot << " is: " << coordinateNumber << endl;
+    } else {
+        cout << "Invalid input! Please enter a valid day and time slot." << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
