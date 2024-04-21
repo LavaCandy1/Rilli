5fbeae6cad2aab86e1198b44410e92b70f43c380
Sherzybath <sdzyrsmy@gmail.com>
2024-04-21 22:11:10 +0530

    Changes

--- csv_to_multi.cpp
+++ csv_to_multi.cpp
@@ -29,6 +29,7 @@ string year = "1nd Year";
 string group = "G1"; //make a code to find group from batch or vise versa
 string batch = "B6";
 
+
 void insert_TimeTable(multiset<Class> TimeTable,string cell){
     auto result = parse_string(cell);
     for(int i=0;i<10;i++){
